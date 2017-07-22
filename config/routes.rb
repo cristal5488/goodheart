5471e39129508c60c39c03111c0c01af02d341be
Cristal <cristal_5488@hotmail.com>
2017-07-22 15:54:07 -0400

    event log created

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
-
+  get '/events/index' => 'events#new'
 end
