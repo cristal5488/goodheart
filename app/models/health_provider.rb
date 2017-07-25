d169af350b427c7fd75aa3b5360474665a3ec007
Cristal <cristal_5488@hotmail.com>
2017-07-25 12:34:58 -0400

    twilio and uniqueness verification

--- app/models/health_provider.rb
+++ app/models/health_provider.rb
@@ -1,8 +1,9 @@
 class HealthProvider < ApplicationRecord
   has_secure_password
-  has_many :events 
+  has_many :events
 
   validates_presence_of :facility_name, :address, message: 'this must be completed'
+  validates_uniqueness_of :phone_number
 
   def name
     self.facility_name
---
+++ app/services/message_sender.rb
@@ -0,0 +1,25 @@
+class MessageSender
+  def self.send_messages
+    new.send_messages
+  end
+
+  def initialize
+    @client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"])
+    @phone_numbers = User.all.map(&:phone_number)
+  end
+
+  def send_messages
+    @phone_numbers.each do |ph|
+      @client.messages.create(:from => twilio_number, :to => ph , :body => "Please attend this blood drive event and help save a life."
+
+    end
+  end
+
+
+
+  private
+
+  def twilio_number
+    twilio_number = '+17864012751 '
+  end
+end
