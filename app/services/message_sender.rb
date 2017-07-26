268227bebe027e69ecf7090a2296fe9ab98cf26b
Cristal <cristal_5488@hotmail.com>
2017-07-25 20:13:06 -0400

    functioning twilio

--- app/services/message_sender.rb
+++ app/services/message_sender.rb
@@ -1,17 +1,17 @@
 class MessageSender
+# MessageSender.send_messages
   def self.send_messages
     new.send_messages
   end
 
   def initialize
     @client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"], ENV["TWILIO_AUTH_TOKEN"])
-    @phone_numbers = User.all.map(&:phone_number)
+    @phones = Donor.all.map(&:phone)
   end
 
   def send_messages
-    @phone_numbers.each do |ph|
-      @client.messages.create(:from => twilio_number, :to => ph , :body => "Please attend this blood drive event and help save a life."
-
+    @phones.each do |ph|
+      @client.messages.create(:from => twilio_number, :to => ph , :body => "Please attend this blood drive event and help save lives.")
     end
   end
 
