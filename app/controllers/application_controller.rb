56df003e9eb0728025810d800427cc25e225b160
Cristal <cristal_5488@hotmail.com>
2017-07-29 21:38:48 -0400

    udpate edit page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  helper_method :current_donor, :current_provider
 
   def current_donor
     @current_donor||= Donor.find_by(id: session[:donor_id]) if session[:donor_id]
   end
+
   def current_provider
     @current_provider ||= HealthProvider.find_by(id: session[:health_provider_id]) if session[:health_provider_id]
   end
 
-  helper_method :current_donor, :current_provider
 
   def authorize
     redirect_to login_path unless (current_donor or current_provider)
