21e36556e2cc81af35f601bee1d0383aeef38ee9
Monica Bello <mc_bello@bellsouth.net>
2017-07-31 19:11:44 -0400

    Update Code

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,33 +1,35 @@
 document.addEventListener("turbolinks:load", function() {
     // Build the chart
-    Highcharts.chart('analytic', {
+    if ($("#analytics_data").length > 0) {
+      Highcharts.chart('analytic', {
         chart: {
-            plotBackgroundColor: null,
-            plotBorderWidth: null,
-            plotShadow: false,
-            type: 'pie'
+          plotBackgroundColor: null,
+          plotBorderWidth: null,
+          plotShadow: false,
+          type: 'pie'
         },
         title: {
-            text: 'Bloodtype Tracker'
+          text: 'Bloodtype Tracker'
         },
         tooltip: {
-            pointFormat: '{series.name}: <br/> total:<b>{point.y}</b> <br/> percentage: <b>{point.percentage:.1f}%</b>'
+          pointFormat: '{series.name}: <br/> total:<b>{point.y}</b> <br/> percentage: <b>{point.percentage:.1f}%</b>'
         },
         plotOptions: {
-            pie: {
-                allowPointSelect: true,
-                cursor: 'pointer',
-                dataLabels: {
-                    enabled: false
-                },
-                showInLegend: true
-            }
+          pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+              enabled: false
+            },
+            showInLegend: true
+          }
         },
         series: [{
-           name: 'Analytic',
-           colorByPoint: true,
-           data: JSON.parse($("#analytics_data").val())
+          name: 'Analytic',
+          colorByPoint: true,
+          data: JSON.parse($("#analytics_data").val())
         }]
-    });
+      });
+    }
 
 });
